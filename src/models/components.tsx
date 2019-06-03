f23d3601a56d067084db6601781db233f5d485bb
Vinicius Arruda <viniarruda.souza@gmail.com>
2019-06-03 12:34:49 +0000

    interface products

--- src/models/components.tsx
+++ src/models/components.tsx
@@ -19,3 +19,15 @@ export interface ColorProps {
 export interface HeaderProps {
   fixed?: boolean;
 }
+
+export interface Products {
+  id?: number;
+  title?: string;
+  price?: number;
+  picture?: string;
+  description?: string;
+  memory?: string;
+  brand?: string;
+  chipType?: string;
+  quantity?: number;
+}
